349b0cd878e72a8c58a3bdb5fe6e9b800ce09dcb
benkeeping <ben@agilex.co.uk>
2012-05-01 18:21:53 +0100

    add group of stuff

---
+++ src/Restful.Wiretypes/GroupOf.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Restful.Wiretypes
+{
+    public class GroupOf<T>
+    {
+        public GroupOf()
+        {
+            Items = new List<T>();
+        }
+        public string GroupName { get; set; }
+        public List<T> Items { get; set; }
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+    }
+}
