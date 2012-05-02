96492b17e50b41135ec616ad8719203ee35db800
benkeeping <ben@agilex.co.uk>
2012-05-02 10:30:58 +0100

    add viewable to field class

--- src/Restful.Wiretypes/Field.cs
+++ src/Restful.Wiretypes/Field.cs
@@ -5,17 +5,28 @@ namespace Restful.Wiretypes
     public class Field<T>
     {
         public T Value { get; set; }
+        public bool Viewable { get; set; }
         public bool Editable { get; set; }
         public List<T> Options { get; set; }
 
+        public static Field<T> From<T>(T value, bool editable, bool viewable, List<T> options)
+        {
+            return new Field<T> { Editable = editable, Viewable = viewable, Value = value, Options = options };
+        }
+
         public static Field<T> From<T>(T value, bool editable, List<T> options)
         {
-            return new Field<T> { Editable = editable, Value = value, Options = options };
+            return Field<T>.From(value, editable, true, new List<T>());
         }
 
         public static Field<T> From(T value, bool editable)
         {
             return Field<T>.From(value, editable, new List<T>());
+        }
+
+        public static Field<T> From(T value, bool editable, bool viewable)
+        {
+            return Field<T>.From(value, editable, viewable, new List<T>());
         }
 
         public override string ToString()
