59eaf8f49c704d4d547d2dc4d801f2cc896a3ed5
benkeeping <ben@agilex.co.uk>
2012-07-31 13:43:20 +0100

    add key-value options for Field

--- src/Restful.Wiretypes/Field.cs
+++ src/Restful.Wiretypes/Field.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Restful.Wiretypes
 {
@@ -7,26 +8,36 @@ namespace Restful.Wiretypes
         public T Value { get; set; }
         public bool Viewable { get; set; }
         public bool Editable { get; set; }
-        public List<T> Options { get; set; }
+        public List<KeyValuePair<T, T>> Options { get; set; }
 
-        public static Field<T> From<T>(T value, bool editable, bool viewable, List<T> options)
+        public static Field<T> From(T value, bool editable, bool viewable, List<T> options)
+        {
+            return new Field<T> { Editable = editable, Viewable = viewable, Value = value, Options = options.Select(x => new KeyValuePair<T,T>(x, x)).ToList() };
+        }
+
+        public static Field<T> From(T value, bool editable, List<T> options)
+        {
+            return Field<T>.From(value, editable, true, options);
+        }
+
+        public static Field<T> From(T value, bool editable, bool viewable, List<KeyValuePair<T, T>> options)
         {
             return new Field<T> { Editable = editable, Viewable = viewable, Value = value, Options = options };
         }
 
-        public static Field<T> From<T>(T value, bool editable, List<T> options)
+        public static Field<T> From(T value, bool editable, List<KeyValuePair<T, T>> options)
         {
             return Field<T>.From(value, editable, true, options);
         }
 
         public static Field<T> From(T value, bool editable)
         {
-            return Field<T>.From(value, editable, new List<T>());
+            return Field<T>.From(value, editable, new List<KeyValuePair<T, T>>());
         }
 
         public static Field<T> From(T value, bool editable, bool viewable)
         {
-            return Field<T>.From(value, editable, viewable, new List<T>());
+            return Field<T>.From(value, editable, viewable, new List<KeyValuePair<T, T>>());
         }
 
         public override string ToString()
